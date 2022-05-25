30d06cc6d6e26ba858b984d7147fa3c55f698512
NilEis <52820950+NilEis@users.noreply.github.com>
2022-05-25 15:14:06 +0200

    initial commit

---
+++ cpuid/cpu_id.h
@@ -0,0 +1,7 @@
+#ifndef CPUID_H
+#define CPUID_H
+#include <stdint.h>
+
+int get_cpuid();
+
+#endif // CPUID_H
---
+++ cpuid/cpuid.c
@@ -0,0 +1,9 @@
+#include "cpu_id.h"
+#ifdef __GNUC__
+#include <cpuid.h>
+
+int get_cpuid()
+{
+    
+}
+#endif // __GNUC__
